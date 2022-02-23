c3d8e5ebb47163e9553ff676013aff1f3f1cf2fe
jay <jaeykweon@naver.com>
2022-02-24 01:20:31 +0900

    draft: insert mock data

--- src/pages/board.js
+++ src/pages/board.js
@@ -1,11 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import boardData from '../assets/test_data/board_data.js';
 
 function Board(){
 
     useEffect(()=> {
         if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1" || window.location.hostname === "") {
         }
-    },[])
+    },[]);
+
+    let [mainData, setMainData] = useState(boardData);
 
     return (
         <>
@@ -59,62 +62,65 @@ function Board(){
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr className="">
-                                    <td className="w-10 px-2 py-2 border-gray-200 bg-white text-sm">
-                                        <div className="w-10 h-10">
-                                            <img 
-                                                className="w-full h-full rounded-full"
-                                                src="https://yt3.ggpht.com/ytc/AKedOLS6RbbzFGvzu_XoKkP17KPrwnej1YvIWw-aTsdm=s88-c-k-c0x00ffffff-no-rj"
-                                                alt="" 
-                                            />
-                                        </div>
-                                    </td>
-                                    <td className="text-center max-w-1/5 px-2 py-2 border-b border-gray-200 bg-white text-sm">
-                                        <p className="text-gray-900 whitespace-no-wrap">
-                                            동빈 나
-                                        </p>
-                                    </td>
+                                    {
+                                        mainData.map((each,i)=> {
+                                            return (
+                                                <tr className="">
+                                                    <td className="w-10 px-2 py-2 border-gray-200 bg-white text-sm">
+                                                        <div className="w-10 h-10">
+                                                            <img 
+                                                                className="w-full h-full rounded-full"
+                                                                src={each.profileImage}
+                                                                alt="" 
+                                                            />
+                                                        </div>
+                                                    </td>
+                                                    <td className="text-center max-w-1/5 px-2 py-2 border-b border-gray-200 bg-white text-sm">
+                                                        <p className="text-gray-900 whitespace-no-wrap">
+                                                            {each.name}
+                                                        </p>
+                                                    </td>
+                                                
+                                                    <td className="w-1/5 px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                        <div className="flex-wrap items-center">
+                                                            {each.tags}
+                                                        </div>
+                                                    </td>
+                                                    <td className="w-96 px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                        
+                                                        {
+                                                            each.videos.map((video)=> 
+                                                                {
+                                                                    return (
+                                                                        <p className="my-2 text-gray-900 whitespace-no-wrap">
+                                                                            {(video)}
+                                                                        </p>
+                                                                    )
+                                                                }
+                                                            )
+                                                        }
+                                                        
+                                                    </td>
+                                                    <td className="w-12 px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                        <p className="text-gray-900 whitespace-no-wrap">
+                                                            2022-02-20T04:30
+                                                        </p>
+                                                    </td>
+                                                    <td className="w-12 px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                                                        <p className="text-gray-900 whitespace-no-wrap">
+                                                            148000
+                                                        </p>
+                                                        {/* <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                                                            <span aria-hidden
+                                                                className="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
+                                                            <span className="relative">Activo</span>
+                                                        </span> */}
+                                                    </td>
+                                                </tr>
+                                            );
+                                        })
+                                    }
                                     
-                                    <td className="w-1/5 px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                        <div className="flex-wrap items-center">
-                                            #python, #AI, #알고리즘,#python, #AI, #알고리즘,#python, #AI, #알고리즘,#python, #AI, #알고리즘,#python, #AI, #알고리즘,#python, #AI, #알고리즘,#python, #AI, #알고리즘,
-                                        </div>
-                                    </td>
-                                    <td className="w-96 px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                        <p className="my-2 text-gray-900 whitespace-no-wrap">
-                                            (이코테 2021) 이것이 취업을 위한 코딩 테스트다 with 파이썬
-                                        </p>
-                                        <p className="my-2 text-gray-900 whitespace-no-wrap">
-                                            꼼꼼한 딥러닝 논문 리뷰와 코드 실습(Deep Learning Paper Review and Practice)
-                                        </p>
-                                        <p className="my-2 text-gray-900 whitespace-no-wrap">
-                                            [라이브 강의] 이것이 취업을 위한 코딩 테스트다 with Python
-                                        </p>
-                                        <p className="my-2 text-gray-900 whitespace-no-wrap">
-                                            청춘 갈아넣기
-                                        </p>
-                                        <p className="my-2 text-gray-900 whitespace-no-wrap">
-                                            코딩 테스트 & 알고리즘 대회 핵심 노트
-                                        </p>
-                                    </td>
-                                    <td className="w-12 px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                        <p className="text-gray-900 whitespace-no-wrap">
-                                            2022-02-20T04:30
-                                        </p>
-                                    </td>
-                                    <td className="w-12 px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                        <p className="text-gray-900 whitespace-no-wrap">
-                                            148000
-                                        </p>
-                                        {/* <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                                            <span aria-hidden
-                                                className="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
-                                            <span className="relative">Activo</span>
-                                        </span> */}
-                                    </td>
-                                </tr>
-
-                                
                             </tbody>
                         </table>
                         <div
