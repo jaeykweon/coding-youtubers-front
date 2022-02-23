56ea38bb724587f490db2746f9c4970c51e335d5
jay <jaeykweon@naver.com>
2022-02-24 01:19:01 +0900

    etc: 파일 위치 변경

--- public/test_data/board_data.js
+++
@@ -1,7 +0,0 @@
-const boardData = [
-    {
-        'id' : 1,
-        'title' : '동빈 나',
-        'tags' : "python, AI, algorithm, ML"
-    },
-]
---
+++ src/assets/test_data/board_data.js
@@ -0,0 +1,30 @@
+const boardData = [
+    {
+        'id' : 1,
+        'name' : '동빈 나',
+        'profileImage': "https://yt3.ggpht.com/ytc/AKedOLS6RbbzFGvzu_XoKkP17KPrwnej1YvIWw-aTsdm=s88-c-k-c0x00ffffff-no-rj",
+        'tags' : "파이썬, AI, 알고리즘, 머신러닝, 코딩 테스트, aws lambda, 해킹, React, node, docker, java",
+        'videos' : [
+            "[꼼꼼한 논문 설명] Imbalanced Continual Learning with Partitioning Reservoir Sampling (ECCV 2020)",
+            "[꼼꼼한 논문 설명] Stable SGD: Understanding the Role of Training Regimes in Continual Learning (NIPS 2020)",
+            "[친절한 최신 딥러닝 논문 소개] DALL-E (OpenAI 2021)",
+            "신입 개발자 이력서 작성 방법의 모든 것 2부 with 코딩몬스터TV",
+            "신입 개발자 이력서 작성 방법의 모든 것 1부 with 코딩몬스터TV (나동빈 이력서 공개 처형)"
+        ]
+    },
+    {
+        'id' : 2,
+        'name' : '코딩몬스터TV',
+        'profileImage': "https://yt3.ggpht.com/ytc/AKedOLQq95jP85AmJcwPOEhxjDE_gkLVFxrxPosz5r8=s88-c-k-c0x00ffffff-no-rj",
+        'tags' : "알고리즘, 면접, 이력서, 병역특례, 딥러닝, 이력서, 포트폴리오, 이직",
+        'videos' : [
+            "시니어 개발자가 말하는 환경 극복 성장 방법 #Shorts",
+            "고졸 개발자는 어떨까? 삼성전자부터 스타트업까지 다녀 본 후기",
+            "신입 개발자가 카카오 경력채용에 합격한 비법은? - 개발자 이력서 분석하기",
+            "주니어 임베디드 개발자의 힘들었던 구직 스토리",
+            "취업준비 하시는 분들에게 하고싶던 말들"
+        ]
+    },
+]
+
+export default boardData;
