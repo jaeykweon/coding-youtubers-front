d9cfa211ab63246018a80bb3c3049eee6cca770c
jay <jaeykweon@naver.com>
2022-03-08 17:06:28 +0900

    etc: 모의 데이터 추가

--- src/assets/test_data/board_data.js
+++ src/assets/test_data/board_data.js
@@ -2,6 +2,7 @@ const boardData = [
     {
         'id' : 1,
         'name' : '동빈 나',
+        'channel_id' :  'UChflhu32f5EUHlY7_SetNWw',
         'profileImage': "https://yt3.ggpht.com/ytc/AKedOLS6RbbzFGvzu_XoKkP17KPrwnej1YvIWw-aTsdm=s88-c-k-c0x00ffffff-no-rj",
         'tags' : "파이썬, AI, 알고리즘, 머신러닝, 코딩 테스트, aws lambda, 해킹, React, node, docker, java",
         'videos' : [
@@ -26,9 +27,15 @@ const boardData = [
     {
         'id' : 2,
         'name' : '코딩몬스터TV',
+        'channel_id' :  'UCaov5NV_OlqUIDuAY2D952Q',
         'profileImage': "https://yt3.ggpht.com/ytc/AKedOLQq95jP85AmJcwPOEhxjDE_gkLVFxrxPosz5r8=s88-c-k-c0x00ffffff-no-rj",
         'tags' : "알고리즘, 면접, 이력서, 병역특례, 딥러닝, 이력서, 포트폴리오, 이직",
         'videos' : [
+            {
+                "title" : "머신러닝, 딥러닝 대체 뭘 공부하라는거야? (+ 무료 교육과정 추천)",
+                "thumbnail": "https://i.ytimg.com/vi/Q2PzllwL51g/sddefault.jpg",
+                "url" : "https://www.youtube.com/watch?v=Q2PzllwL51g"
+            },
             {
                 "title" : "시니어 개발자가 말하는 환경 극복 성장 방법 #Shorts",
                 "thumbnail": "https://i.ytimg.com/vi/50XmEnOeIPo/sddefault.jpg",
@@ -39,16 +46,35 @@ const boardData = [
                 "thumbnail": "https://i.ytimg.com/vi/RuXAvPSJi3A/sddefault.jpg",
                 "url" : "https://www.youtube.com/watch?v=RuXAvPSJi3A"
             },
-            {
-                "title" : "신입 개발자가 카카오 경력채용에 합격한 비법은? - 개발자 이력서 분석하기",
-                "thumbnail": "https://i.ytimg.com/vi/8xvYz0ldfEI/sddefault.jpg",
-                "url" : "https://www.youtube.com/watch?v=8xvYz0ldfEI"
-            },
-            
-            
         ],
         'subscriberCount': 8410
     },
+    {
+        'id' : 3,
+        'name' : '널널한 개발자 TV',
+        'channel_id' :  'UCdGTtaI-ERLjzZNLuBj3X6A',
+        'profileImage': "https://yt3.ggpht.com/poGhRWCvYdXFDGO3oUdE86Z0so1ygV8aTPZF4z7xpaySuG0cdBuFuk_fxDoTwVM--7FZ8tBW0g=s88-c-k-c0x00ffffff-no-rj",
+        'tags' : "C++, 자료구조, 알고리즘, 운영체제, 네트워크",
+        'videos' : [
+            {
+                "title" : "WAS, JVM 그리고 RESTful API",
+                "thumbnail": "https://i.ytimg.com/vi/poKkQHUBt9A/hqdefault.jpg",
+                "url" : "https://www.youtube.com/watch?v=poKkQHUBt9A"
+            },
+            {
+                "title" : "웹 서비스 3대 요소",
+                "thumbnail": "https://i.ytimg.com/vi/byR3BcrChT8/hqdefault.jpg",
+                "url" : "https://www.youtube.com/watch?v=byR3BcrChT8"
+            },
+            {
+                "title" : "초창기 웹 서비스 구조",
+                "thumbnail": "https://i.ytimg.com/vi/4Sfned8HLzk/hqdefault.jpg",
+                "url" : "https://www.youtube.com/watch?v=4Sfned8HLzk"
+            },
+        ],
+        'subscriberCount': 16200
+    },
+    
 ]
 
 export default boardData;
